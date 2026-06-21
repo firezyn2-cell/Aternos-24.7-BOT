549ce74d380ee88171eff63249d06ce15b39d1d1
firezyn2-cell <firezyn2@gmail.com>
2026-06-21 16:51:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mineflayer = require('mineflayer');
 
-const serverHost = process.env.SERVER_HOST || 'DOOMS_DAY_REBORN.aternos.me';
-const serverPort = parseInt(process.env.SERVER_PORT || '59173', 10);
-const botUsername = process.env.BOT_USERNAME || '247_Monitor';
+const serverHost = process.env.SERVER_HOST || 'LS22.aternos.me';
+const serverPort = parseInt(process.env.SERVER_PORT || '50873', 10);
+const botUsername = process.env.BOT_USERNAME || '24/7 BOT';
 const minecraftVersion = process.env.MC_VERSION || false;
 const reconnectInterval = parseInt(process.env.RECONNECT_INTERVAL_MS || '40000', 10);
 const antiAfkInterval = parseInt(process.env.ANTI_AFK_INTERVAL_MS || '20000', 10);
-const httpPort = parseInt(process.env.PORT || '3000', 10);
+const httpPort = parseInt(process.env.PORT || '50873', 10);
 
 const app = express();
 const server = http.createServer(app);
